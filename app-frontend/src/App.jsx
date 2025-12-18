0dffc86688c0f4465e85ff94a4990376a131386b
Shrishail <shrishailchincholi306@gmail.com>
2025-12-18 22:11:27 +0530

    Reaggraged Code

--- app-frontend/src/App.jsx
+++ app-frontend/src/App.jsx
@@ -7,6 +7,7 @@ import "./style/spend.css"
 import "./style/month.css"
 import "./style/download.css"
 import "./style/dashboard.css"
+import "./style/login&page.css"
 import Navbar from "./components/Navbar"
 import AddNote from "./pages/Addnotes"
 import AddExpense from "./pages/AddExpense"
@@ -17,6 +18,7 @@ import SpendingLimits from "./pages/spendLimt"
 import MonthlyReport from "./pages/monthlyreport"
 import DownloadReport from "./pages/downloadreport"
 import Dashboard from "./pages/dashboard"
+import Register from "./pages/login&page"
 
 function App() {
 
@@ -26,7 +28,7 @@ function App() {
         <Navbar />
 
         <Routes>
-          <Route path="/" element={<Dashboard />} />
+          <Route path="/" element={<Register />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/expenses/add" element={<AddExpense />} />
           <Route path="/expenses/all" element={<TotalExpenses />} />
