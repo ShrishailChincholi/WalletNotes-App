554b65b167bdef13836addec3ab0d5dd2bf6788e
Shrishail <shrishailchincholi306@gmail.com>
2025-12-28 22:31:43 +0530

    editeprofiel .jsx file added

---
+++ app-frontend/src/pages/editeprofile.jsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const EditProfile = () => {
+  const navigate = useNavigate();
+  const token = localStorage.getItem("token");
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: ""
+  });
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user) {
+      // eslint-disable-next-line react-hooks/set-state-in-effect
+      setFormData({
+        name: user.name,
+        email: user.email,
+        password: ""
+      });
+    }
+  }, []);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const res = await fetch("http://localhost:6060/user/update", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`
+      },
+      body: JSON.stringify(formData)
+    });
+
+    const data = await res.json();
+
+    if (data.success) {
+      localStorage.setItem("user", JSON.stringify(data.user));
+      alert("Profile Updated!");
+      navigate("/account");
+    } else {
+      alert(data.message);
+    }
+  };
+
+  return (
+    <div className="register-form">
+      <h2>Edit Profile</h2>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          placeholder="Name"
+          onChange={handleChange}
+          required
+        />
+
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          placeholder="Email"
+          onChange={handleChange}
+          required
+        />
+
+        <input
+          type="password"
+          name="password"
+          placeholder="New Password (optional)"
+          onChange={handleChange}
+        />
+
+        <button type="submit">Update</button>
+      </form>
+    </div>
+  );
+};
+
+export default EditProfile;
